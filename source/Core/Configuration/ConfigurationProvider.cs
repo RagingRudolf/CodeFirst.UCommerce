8fbf0475da7aa24c45f9181df292034fafd9f7f8
RagingRudolf <martin_k2001@hotmail.com>
2014-08-12 19:02:16 +0200

    Asserting configuration has been added to app.config/web.config. Asserting assemblyt name.

--- source/Core/Configuration/ConfigurationProvider.cs
+++ source/Core/Configuration/ConfigurationProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Configuration;
+using System.IO;
 using System.Reflection;
 
 using RagingRudolf.CodeFirst.UCommerce.Core.Extensions;
@@ -16,10 +17,25 @@ namespace RagingRudolf.CodeFirst.UCommerce.Core.Configuration
 
 		public Assembly GetAssembly()
 		{
-			string assemblyName = _configuration != null && _configuration.AssemblyName.IsNotEmpty()
-				? _configuration.AssemblyName
-				: "RagingRudolf.CodeFirst.UCommerce.Models";
-			var assembly = Assembly.Load(assemblyName);
+			if (_configuration == null)
+				throw new ConfigurationErrorsException(
+					"Misconfiguration! Could not find ConfigurationSection 'RagingRudolf/CodeFirst' " +
+					"in web.config or app.config. Make sure it has been added and configured.");
+			
+			if (_configuration.AssemblyName.IsEmpty())
+				throw new ConfigurationErrorsException(
+					"Misconfiguration! assemblyname attribute for ConfigurationSection 'RagingRudolf/CodeFirst' " +
+					"is not allowed to be null or empty. Make sure to define a valid assembly name.");
+
+			Assembly assembly = null;
+
+			try
+			{
+				assembly = Assembly.Load(_configuration.AssemblyName);
+			} catch(FileNotFoundException ex) {
+				throw new ConfigurationErrorsException(
+					string.Format("Misconfiguration! Configured assembly with name '{0}' could not be found.", _configuration.AssemblyName), ex);
+			}
 
 			return assembly;
 		}
