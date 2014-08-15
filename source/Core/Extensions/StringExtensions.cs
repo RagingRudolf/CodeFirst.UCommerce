37f62addba666b3b10cc150efcd455aeca7d6a32
RagingRudolf <martin_k2001@hotmail.com>
2014-08-15 20:40:42 +0200

    Adding tests project with tests for string extensions.

--- source/Core/Extensions/StringExtensions.cs
+++ source/Core/Extensions/StringExtensions.cs
@@ -2,10 +2,10 @@
 {
 	public static class StringExtensions
 	{
-		 public static bool IsEmpty(this string str)
-		 {
-			 return string.IsNullOrWhiteSpace(str);
-		 }
+		public static bool IsEmpty(this string str)
+		{
+			return string.IsNullOrWhiteSpace(str);
+		}
 
 		public static bool IsNotEmpty(this string str)
 		{
