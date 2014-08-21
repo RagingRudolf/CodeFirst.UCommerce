8d2cfc1c971b02e5df98f2153da4376c0e07e8ec
RagingRudolf <martin_k2001@hotmail.com>
2014-08-21 19:13:38 +0200

    Added support for campaign item fields.

--- source/Core/Handlers/CampaignDefinitionHandler.cs
+++ source/Core/Handlers/CampaignDefinitionHandler.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 using NHibernate;
 
 using RagingRudolf.CodeFirst.UCommerce.Core.Attributes;
@@ -26,8 +29,9 @@ namespace RagingRudolf.CodeFirst.UCommerce.Core.Handlers
 		public void Handle(Type type)
 		{
 			Definition definition = HandleDefinition(_session, type);
+			Definition addedFields = HandleFieldTypes(_session, type, definition);
 
-			_session.SaveOrUpdate(definition);
+			_session.SaveOrUpdate(addedFields);
 		}
 
 		private static Definition HandleDefinition(ISession session, Type type)
@@ -68,6 +72,73 @@ namespace RagingRudolf.CodeFirst.UCommerce.Core.Handlers
 			definition.Description = attribute.Description.IsNotEmpty()
 				? attribute.Description
 				: string.Empty;
+
+			return definition;
+		}
+
+		private static Definition HandleFieldTypes(ISession session, Type type, Definition definition)
+		{
+			IEnumerable<PropertyInfo> properties = type.GetAttributedProperties<CategoryDefinitionFieldAttribute>();
+
+			foreach (PropertyInfo propertyInfo in properties)
+			{
+				var attribute = propertyInfo.AssertGetCustomAttribute<CategoryDefinitionFieldAttribute>();
+				string name = attribute.Name.IsNotEmpty()
+					? attribute.Name
+					: propertyInfo.Name;
+
+				var field = definition.DefinitionFields
+					.SingleOrDefault(x => x.Name == name);
+
+				if (field == null)
+				{
+					var dataType = session
+						.QueryOver<DataType>()
+						.Where(x => x.TypeName == attribute.DataType)
+						.SingleOrDefault<DataType>();
+
+					field = new DefinitionField
+					{
+						Name = name,
+						DataType = dataType,
+						Definition = definition
+					};
+
+					if (definition.DefinitionFields == null)
+						definition.DefinitionFields = new Collection<DefinitionField>();
+
+					definition.DefinitionFields.Add(field);
+				}
+
+				field.DisplayOnSite = attribute.DisplayOnSite;
+				field.Multilingual = attribute.Multilingual;
+				field.RenderInEditor = attribute.RenderInEditor;
+				field.Searchable = attribute.Searchable;
+				field.SortOrder = attribute.SortOrder;
+				field.DefaultValue = attribute.DefaultValue;
+
+				IEnumerable<LanguageDescriptionAttribute> descriptions = propertyInfo.GetCustomAttributes<LanguageDescriptionAttribute>();
+
+				foreach (var description in descriptions)
+				{
+					DefinitionFieldDescription fieldDescription = field.DefinitionFieldDescriptions
+						.SingleOrDefault(x => x.CultureCode == description.Language);
+
+					if (fieldDescription == null)
+					{
+						fieldDescription = new DefinitionFieldDescription
+						{
+							DefinitionField = field,
+							CultureCode = description.Language,
+						};
+
+						field.AddDescription(fieldDescription);
+					}
+
+					fieldDescription.DisplayName = description.DisplayName;
+					fieldDescription.Description = description.Description ?? string.Empty;
+				}
+			}
 
 			return definition;
 		}
--- source/Models/CampaignDefinitions/SuperCampaign.cs
+++ source/Models/CampaignDefinitions/SuperCampaign.cs
@@ -5,5 +5,8 @@ namespace RagingRudolf.Examples.Models.CampaignDefinitions
 	[CampaignDefinition("Super Campaign", Description = "This is a super campaign for awesome on sales item!")]
 	public class SuperCampaign
 	{
+		[CategoryDefinitionField("DebitorGroup", "Number", DefaultValue = "1", RenderInEditor = true)]
+		[LanguageDescription("da-DK", "Debitor gruppe",  Description = "Gruppe id'et, som kan modtage denne rabat.")]
+		public string DebitorGroup { get; set; }
 	}
 }
