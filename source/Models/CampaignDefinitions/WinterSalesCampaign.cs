3dc0d2c3760358c98d731553b1f7899c6ce0cd2c
Martin Askjær Kristensen <martin_k2001@hotmail.com>
2016-04-03 13:10:31 +0200

    Add WinterSalesCampaign definition example

---
+++ source/Models/CampaignDefinitions/WinterSalesCampaign.cs
@@ -0,0 +1,12 @@
+﻿using RagingRudolf.UCommerce.CodeFirst.Core;
+using RagingRudolf.UCommerce.CodeFirst.Core.Attributes.Shared;
+
+namespace RagingRudolf.UCommerce.CodeFirst.Examples.CampaignDefinitions
+{
+    [Definition(BuiltInDefinitionType.CampaignItem, "Winter sales", "Winter is coming up with some cool prices.")]
+    public class WinterSalesCampaign
+    {
+        [Field("PremiumMembersOnly", Constants.BuiltInDataTypes.Boolean, RenderInEditor = true)]
+        public bool PremiumMembersOnly { get; set; }
+    }
+}
